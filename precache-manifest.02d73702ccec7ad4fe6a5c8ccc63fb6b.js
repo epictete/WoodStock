49a7af0b42291e8ce5accbbc2a43f7bb8c085ab2
epictete <john.laterre@gmail.com>
2020-10-14 18:01:03 +0200

    Updates

---
+++ precache-manifest.02d73702ccec7ad4fe6a5c8ccc63fb6b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "347fdf1e4c4172f25dbff3317711fb9f",
+    "url": "/WoodStock/index.html"
+  },
+  {
+    "revision": "5621df863fd3ee80bd1f",
+    "url": "/WoodStock/static/css/main.7ea4629c.chunk.css"
+  },
+  {
+    "revision": "8a467673bbcad79bb1f3",
+    "url": "/WoodStock/static/js/2.a85eb3ef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/WoodStock/static/js/2.a85eb3ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5621df863fd3ee80bd1f",
+    "url": "/WoodStock/static/js/main.467c3f37.chunk.js"
+  },
+  {
+    "revision": "b28fda7c55ce626a96f7",
+    "url": "/WoodStock/static/js/runtime-main.39750586.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/WoodStock/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/WoodStock/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/WoodStock/static/media/spinner.235b9b3e.gif"
+  }
+]);
